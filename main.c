f810766a01f024a5afc5f63737dcd8c490758cab
Prapan Chulapinyo <nquantum@hotmail.com>
2017-07-29 23:47:35 +0700

    initial one

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: nquan
+ *
+ * Created on July 29, 2017, 11:39 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=apsa_tester.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/apsa_tester.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=apsatester.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/apsatester.x.tar
